b2924b99f503dcff3c11c2060f1ca8d7641570c2
Álvaro Gordillo <alvaro.gordillo.reina@gmailcom>
2019-07-17 13:30:22 +0200

    Modals Create Owners

--- src/containers/Owner/CreateOwner/CreateOwner.jsx
+++ src/containers/Owner/CreateOwner/CreateOwner.jsx
@@ -3,6 +3,11 @@ import Input from "../../../UI/Inputs/Input";
 import { Form, Card, Col, Button, Row } from "react-bootstrap";
 import { returnInputConfiguration } from "../../../Utility/InputConfiguration";
 import * as formUtilityActions from "../../../Utility/FormUtility";
+import { connect } from "react-redux";
+import * as repositoryActions from "../../../store/actions/repositoryActions";
+import * as errorHandlerActions from "../../../store/actions/errorHandlerActions";
+import SuccessModal from "../../../components/Modals/SuccessModal/SuccessModal";
+import ErrorModal from "../../../components/Modals/ErrorModal/ErrorModal";
 
 class CreateOwner extends Component {
   constructor(props) {
@@ -24,6 +29,23 @@ class CreateOwner extends Component {
     const counter = formUtilityActions.countInvalidElements(updatedOwnerForm);
 
     this.setState({ ownerForm: updatedOwnerForm, isFormValid: counter === 0 });
+  };
+
+  createOwner = event => {
+    event.preventDefault();
+
+    const ownerToCreate = {
+      name: this.state.ownerForm.name.value,
+      address: this.state.ownerForm.address.value,
+      dateOfBirth: this.state.ownerForm.dateOfBirth.value
+    };
+
+    const url = "/api/owner";
+    this.props.onCreateOwner(url, ownerToCreate, { ...this.props });
+  };
+
+  redirectToOwnerList = () => {
+    this.props.history.push("/owner-List");
   };
 
   render() {
@@ -71,10 +93,47 @@ class CreateOwner extends Component {
               </Col>
             </Form.Group>
           </Form>
+          <SuccessModal
+            show={this.props.showSuccessModal}
+            modalHeaderText={"Success message"}
+            modalBodyText={"Action completed successfully"}
+            okButtonText={"OK"}
+            successClick={() =>
+              this.props.onCloseSuccessModal("/owner-List", { ...this.props })
+            }
+          />
+          <ErrorModal
+            show={this.props.showErrorModal}
+            modalHeaderText={"Error message"}
+            modalBodyText={this.props.errorMessage}
+            okButtonText={"OK"}
+            closeModal={() => this.props.onCloseErrorModal()}
+          />
         </Card.Body>
       </Card>
     );
   }
 }
 
-export default CreateOwner;
+const mapStateToProps = state => {
+  return {
+    showSuccessModal: state.repository.showSuccessModal,
+    showErrorModal: state.errorHandler.showErrorModal,
+    errorMessage: state.errorHandler.errorMessage
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onCreateOwner: (url, owner, props) =>
+      dispatch(repositoryActions.postData(url, owner, props)),
+    onCloseSuccessModal: (url, props) =>
+      dispatch(repositoryActions.closeSuccessModal(props, url)),
+    onCloseErrorModal: () => dispatch(errorHandlerActions.closeErrorModal())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CreateOwner);
